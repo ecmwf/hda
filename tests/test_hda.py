fb7342e2c45b898ff37a3fbbb81c5fddba0ca4d1
Germano Guerrini <germano.guerrini@exprivia.com>
2022-10-18 17:35:40 +0200

    Fixed test assertion

--- tests/test_hda.py
+++ tests/test_hda.py
@@ -58,7 +58,7 @@ def test_hda_1():
     }
     matches = c.search(r)
     print(matches)
-    assert len(matches.results) == 1, matches
+    assert len(matches.results) == 940, matches
     # Too large to download
     # matches.download()
 
