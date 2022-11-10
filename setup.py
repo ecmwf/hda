c7ee4888564a020a44e3787122b02821975fc022
ricardo-correa <47784603+ricardo-correa@users.noreply.github.com>
2022-11-10 11:28:00 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
-        "requests>=2.5.0", 
+        "requests>=2.5.0",
         "tqdm",
     ],
     zip_safe=True,
